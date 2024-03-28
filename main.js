42fd65a1ef7838f9f2f6001ef4304aeb430ba84e
BeetrootAcademyAccount2 <beetroot444@yahoo.com>
2024-03-28 22:08:58 +0200

    Add files

---
+++ main.js
@@ -0,0 +1,51 @@
+//This is a one line comment
+
+/*
+Multi-line comment 
+*/
+//var, let, const
+
+let myName; // declaration
+
+myName = "Ivo"; // initialization /defining
+/*
+console.log(myName);
+*/
+
+// document.body.innerHTML = "<h2>New smaller title</h2>";
+
+//document.querySelector("h1").style.color = "red";
+
+/*
+const pi = 3.14;
+
+let r = 5;
+
+r = 4;
+
+r = 3;
+
+const area = pi * r * r;
+
+console.info("This is info");
+console.warn("This is a warning");
+console.error("This is an error");
+console.debug("This is debug");
+console.clear();
+*/
+
+/*
+console.table([
+  { name: "John", age: 23 },
+  { name: "Steven", age: 30 },
+]);
+*/
+
+/*
+const myName = prompt("Please enter your name:");
+console.log("Hello Mr. " + myName);
+document.body.innerHTML = "<h2>Hello Mr. " + myName + "</h2>";
+*/
+
+const shopName = `${myName} repair shop for tvs`;
+console.log(shopName);
